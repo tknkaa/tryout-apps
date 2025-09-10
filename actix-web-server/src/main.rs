697b06429cea2a96a8968c1dc7c172011f60bb8f
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-10 23:12:41 +0800

    handle shared mutable state

--- actix-web-server/src/main.rs
+++ actix-web-server/src/main.rs
@@ -1,24 +1,28 @@
-use actix_web::{App, HttpServer, get, web};
+use actix_web::{App, HttpServer, web};
 use std::io;
+use std::sync::Mutex;
 
-struct AppState {
-    app_name: String,
+struct AppStateWithCounter {
+    // The server can handle many requests at once.
+    counter: Mutex<i32>,
 }
 
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}!")
+async fn index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Request number: {counter}")
 }
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+    // `move` forces the closure to take ownership of the variables it uses.
+    HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix Web"),
-            }))
-            .service(index)
+            .app_data(counter.clone())
+            .route("/", web::get().to(index))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
